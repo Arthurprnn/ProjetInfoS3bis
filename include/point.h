1a8d90fb266733b6795d9849a76fb5425c051a6a
Arthurprnn <arthur.peresson@univ-tlse3.fr>
2019-12-01 16:16:11 +0100

    version2

--- include/point.h
+++ include/point.h
@@ -3,11 +3,35 @@
 #include <stdbool.h>
 
 
-
-struct Coordonnees {
+//structure de la Position
+struct Coordonnees {                
     double x;
     double y;
     double z;
     double t;  // variable du temps
 };
+
+
+//Nom de la structure
 typedef struct Coordonnees Position;
+typedef struct Coordonnees* Coordonnees;
+
+
+//nouvelle coordonnees
+Coordonnees Nouvellepose(float x, float y ,float z, float t);
+
+
+//Recuperer la valeur de x
+float valeur_x(Coordonnees new_pose);
+
+
+//Recuperer la valeur de y
+float valeur_y(Coordonnees new_pose);
+
+
+//Recuperer la valeur de z
+float valeur_z(Coordonnees new_pose);
+
+
+//Recuperer la valeur de t
+float valeur_t(Coordonnees new_pose);
--- src/Point/point.c
+++ src/Point/point.c
@@ -3,3 +3,44 @@
 #include <stdbool.h>
 #include "../../include/point.h"
 
+
+//nouvelle coordonnees
+Coordonnees Nouvellepose(float x, float y ,float z, float t){
+    Coordonnees new_pose;
+    new_pose=(Coordonnees)malloc(sizeof(Position));
+
+    new_pose->x=x;
+    new_pose->y=y;
+    new_pose->z=z;
+    new_pose->t=t;
+    
+    return new_pose;
+} 
+
+
+//Recuperer la valeur de x
+float valeur_x(Coordonnees new_pose){
+    float x=new_pose->x;
+    return x;
+}
+
+
+//Recuperer la valeur de y
+float valeur_y(Coordonnees new_pose){
+    float y=new_pose->y;
+    return y;
+}
+
+
+//Recuperer la valeur de z
+float valeur_z(Coordonnees new_pose){
+    float z=new_pose->z;
+    return z;
+}
+
+
+//Recuperer la valeur de t
+float valeur_t(Coordonnees new_pose){
+    float t=new_pose->t;
+    return t;
+}
